caf4cf9b7b0949829fae4201ce5d9d429c979f17
shockeyt <shockeyt@gmail.com>
2017-06-01 01:28:22 -0600

    more peaks page styling

--- src/components/Peak.js
+++ src/components/Peak.js
@@ -25,7 +25,9 @@ import { Separator } from 'react-native-form-generator';
 import axios from 'axios';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
-const myIcon = (<Icon name="check-circle-o" size={40} color="#900" />);
+const myIcon = (<Icon name="check-circle-o" size={30} color="#2f62bf" />);
+const arrow = (<Icon name="chevron-left" size={60} color="#234F33" />);
+const compass = (<Icon name="compass" size={15} color="red" />);
 
 const ds = {};
 
@@ -53,6 +55,7 @@ var styles = StyleSheet.create({
     color: 'white',
     backgroundColor: '#234F33',
     marginTop: 0,
+    marginLeft: 30,
     borderRadius: 2
   },
   rowContainer: {
@@ -90,7 +93,7 @@ var styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#8E8E8E',
     marginBottom: 10,
-    marginHorizontal: 10,
+    marginHorizontal: 20,
     backgroundColor: 'white',
   },
 });
@@ -168,22 +171,24 @@ class Peak extends Component {
         <View style={styles.container}> 
         <ScrollView> 
           <Button style={styles.exit} onPress={() => {this.setState({showPeak: false})}}>
-          Back
+          {arrow}
           </Button>
           
           <View style={styles.peak}>
             <View style={styles.completed}>
               
-              <Text style={{marginLeft:100, marginTop:0, fontSize: 18}} onPress={() => {this.props.onSavePeak(this.state.selectedPeak)}}>Completed {myIcon}</Text>
+              <Text style={{marginTop:0, fontSize: 18}} onPress={() => {this.props.onSavePeak(this.state.selectedPeak)}}>Completed? {myIcon}</Text>
             </View>
             <Text style={{fontSize: 30, color: 'white', marginBottom: 10, marginTop: 10}}>{this.state.selectedPeak.name}</Text>
             <Image source={{uri: this.state.selectedPeak.picture}} style={{width: 270, height: 270}}/>
+            <View>
             <Text style={{fontSize: 18, marginTop: 5}}>Elevation: {this.state.selectedPeak.elevation} ft</Text>
             <Text style={{fontSize: 18}}>Rating: {this.state.selectedPeak.rating}</Text>
             <Text style={{fontSize: 18}}>Nearest town: {this.state.selectedPeak.near}</Text>    
             <Text style={{fontSize: 18}}>Round Trip Distance: {this.state.selectedPeak.distance}</Text>
-            <Text style={{width: 270, marginTop: 5, backgroundColor: 'white'}}>Directions: {this.state.selectedPeak.directions}</Text>
-            <Text style={{width: 270, marginTop: 5, backgroundColor: 'white'}}>Route: {this.state.selectedPeak.route}</Text>
+            </View>
+            <Text style={{padding: 5, width: 270, marginTop: 5, backgroundColor: 'white'}}>Directions: {this.state.selectedPeak.directions}</Text>
+            <Text style={{padding: 5, width: 270, marginTop: 5, backgroundColor: 'white'}}>Route: {this.state.selectedPeak.route}</Text>
             
           
           </View>
@@ -209,7 +214,7 @@ class Peak extends Component {
             <Image source={{uri: data.picture}} style={{width: 40, height: 40, borderRadius: 20}} />
             <Text style={styles.rowText} onPress={() => {this.rowClick(data)}}>{data.name}</Text>
               <View style={styles.rowElevation}>
-                <Text style={styles.textElevation}>{data.elevation} ft</Text>
+                <Text style={styles.textElevation}>{compass} {data.elevation} ft</Text>
               </View>
             </View>
           }
@@ -225,96 +230,8 @@ class Peak extends Component {
     );
   }
 
-//   constructor(props) {
-//       super(props);
-
-//       var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-//       this.state = {
-//         data: this._genRow(),
-//         dataSource: ds,
-//       }
-//     }
-
-//     componentDidMount() {
-//       this.setState({
-//         dataSource: this.state.dataSource.cloneWithRows(this.state.data)
-//       });
-//     }
-
-//     _genRow(){
-//       var datas = [];
-//       for (var i = 0; i < 5; i++) {
-//         datas.push({
-//           row: i,
-//           isSelect: false,
-//         });
-//       }
-//       console.log('datas ' + JSON.stringify(datas));
-//       return datas;
-//     }
-
-//     render() {
-//       return (
-//         <ListView
-//           dataSource = {this.state.dataSource}
-//           renderRow = {this._renderRow.bind(this)}
-//           renderHeader = {() => <View style={{height: 10, backgroundColor:     '#f5f5f5'}} />}
-//           onEndReached = {() => console.log('')}
-//           renderSeparator = {(sectionID, rowID) =>
-//             <View
-//               style={styles.style_separator}
-//               key={`${sectionID} - ${rowID}`}
-//             />}
-//         />
-//       );
-//     }
-
-//     _renderRow(rowData: string, sectionID: number, rowID: number) {
-//       console.log('render row ...');
-//       return (
-//         <TouchableHighlight onPress={this._onPressRow.bind(this.rowID, rowData)}>
-//           <View style={styles.style_row_view}>
-//             <Text style={styles.style_text}>{rowData.row}</Text>
-//             <Text style={styles.style_text}>{rowData.isSelect ? 'true' : 'false'}                   </Text>
-//               </View>
-//             </TouchableHighlight>
-//           );
-//         }
-
-//     _onPressRow(rowID, rowData) {
-
-//       rowData.isSelect = !rowData.isSelect;
-//       var dataClone = this.state.data;
-//       dataClone[rowID] = rowData;
-//       this.setState({
-//         data: dataClone
-//       });
-//       console.log(this.state.data);
-//     }
 }
 
-// const styles = StyleSheet.create({
-//   style_row_view: {
-//     flex: 1,
-//     flexDirection: 'row',
-//     height: 57,
-//     backgroundColor: '#FFFFFF',
-//   },
-//   style_text: {
-//     flex: 1,
-//     marginLeft: 12,
-//     fontSize: 16,
-//     color: '#333333',
-//     alignSelf: 'center',
-//   },
 
-// });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     paddingTop: 100,
-//     paddingBottom: 100,
-//   },
-// });
  
 module.exports = Peak;
